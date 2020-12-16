25299e5dde5b9941ba9c39f8bc2804867005871c
Andrey Shilov <andrey.dry7.shilov@yandex.ru>
2020-12-16 09:56:54 +0300

    Add deleteField method

--- src/Api/TemplatesApi.php
+++ src/Api/TemplatesApi.php
@@ -2372,4 +2372,268 @@ class TemplatesApi extends BaseAPI
             $httpBody
         );
     }
+
+    /**
+     * Operation deleteField
+     *
+     * Delete field
+     *
+     * @param  string $template_id Template ID (required)
+     * @param  string $field_id Field ID (required)
+     *
+     * @throws \WalletPassJP\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return void
+     */
+    public function deleteField($template_id, $field_id)
+    {
+        $this->deleteFieldWithHttpInfo($template_id, $field_id);
+    }
+
+    /**
+     * Operation deleteFieldWithHttpInfo
+     *
+     * Delete field
+     *
+     * @param  string $template_id Template ID (required)
+     * @param  string $field_id Field ID (required)
+     *
+     * @throws \WalletPassJP\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of null, HTTP status code, HTTP response headers (array of strings)
+     */
+    private function deleteFieldWithHttpInfo($template_id, $field_id)
+    {
+        $returnType = '';
+        $request = $this->deleteFieldRequest($template_id, $field_id);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse()
+                        ? $e
+                        ->getResponse()
+                        ->getBody()
+                        ->getContents()
+                        : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            return [null, $statusCode, $response->getHeaders()];
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 401:
+                    $data = ObjectSerializer::deserialize(
+                        json_decode($e->getResponseBody()),
+                        '\WalletPassJP\Model\Error',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 403:
+                    $data = ObjectSerializer::deserialize(
+                        json_decode($e->getResponseBody()),
+                        '\WalletPassJP\Model\Error',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 404:
+                    $data = ObjectSerializer::deserialize(
+                        json_decode($e->getResponseBody()),
+                        '\WalletPassJP\Model\Error',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 500:
+                    $data = ObjectSerializer::deserialize(
+                        json_decode($e->getResponseBody()),
+                        '\WalletPassJP\Model\Error',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation deleteFieldAsync
+     *
+     * Delete field
+     *
+     * @param  string $template_id Template ID (required)
+     * @param  string $field_id Field ID (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function deleteFieldAsync($template_id, $field_id)
+    {
+        return $this->deleteFieldAsyncWithHttpInfo($template_id, $field_id)->then(function (
+            $response
+        ) {
+            return $response[0];
+        });
+    }
+
+    /**
+     * Operation deleteFieldAsyncWithHttpInfo
+     *
+     * Delete field
+     *
+     * @param  string $template_id Template ID (required)
+     * @param  string $field_id Field ID (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    private function deleteFieldAsyncWithHttpInfo($template_id, $field_id)
+    {
+        $returnType = '';
+        $request = $this->deleteFieldRequest($template_id, $field_id);
+
+        return $this->client->sendAsync($request, $this->createHttpClientOption())->then(
+            function ($response) use ($returnType) {
+                return [null, $response->getStatusCode(), $response->getHeaders()];
+            },
+            function ($exception) {
+                $response = $exception->getResponse();
+                $statusCode = $response->getStatusCode();
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $exception->getRequest()->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+        );
+    }
+
+    /**
+     * Create request for operation 'deleteField'
+     *
+     * @param  string $template_id Template ID (required)
+     * @param  string $field_id Field ID (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    protected function deleteFieldRequest($template_id, $field_id)
+    {
+        // verify the required parameter 'template' is set
+        if ($template_id === null || (is_array($template_id) && count($template_id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $template_id when calling deleteTemplate'
+            );
+        }
+
+        // verify the required parameter 'field' is set
+        if ($field_id === null || (is_array($field_id) && count($field_id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $field_id when calling deleteField'
+            );
+        }
+
+        $resourcePath = '/templates/{template}/fields/{field}';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+        // path params
+        if ($template_id !== null) {
+            $resourcePath = str_replace('{template}', $template_id, $resourcePath);
+        }
+        if ($field_id !== null) {
+            $resourcePath = str_replace('{field}', $field_id, $resourcePath);
+        }
+
+        // body params
+        $_tempBody = null;
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(['application/json']);
+        } else {
+            $headers = $this->headerSelector->selectHeaders(['application/json'], []);
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            // $_tempBody is the method argument, if present
+            $httpBody = $_tempBody;
+            // \stdClass has no __toString(), so we should encode it manually
+            if (
+                $httpBody instanceof \stdClass &&
+                $headers['Content-Type'] === 'application/json'
+            ) {
+                $httpBody = \GuzzleHttp\json_encode($httpBody);
+            }
+        } elseif (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $multipartContents[] = [
+                        'name' => $formParamName,
+                        'contents' => $formParamValue,
+                    ];
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+            } elseif ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($formParams);
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+            }
+        }
+
+        // // this endpoint requires Bearer token
+        if ($this->config->getAccessToken() !== null) {
+            $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge($defaultHeaders, $headerParams, $headers);
+
+        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        return new Request(
+            'DELETE',
+            $this->config->getEndpoint() . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
 }
